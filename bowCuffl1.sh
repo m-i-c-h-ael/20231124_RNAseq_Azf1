df0bf47db16e843dd3c5010103c8c05f52642005
m-i-c-h-ael <plank.michael.j@gmail.com>
2023-11-28 10:58:37 +0100

    Shell script for Bowtie and Cufflinks

--- bowCuffl1.sh
+++ bowCuffl1.sh
@@ -1,17 +1,32 @@
+# Number of threads to use
+threads=3
+
 # Take letter of sequencing file as input; move to the corresponding directory
 letter=$@
 cd /home/wrtlbrnft/RNAseq/$letter
 
+# Unzip read files - not necessary for bowtie
+#gunzip *.qfq.gz
+
 # Align reads to reference genome
-bowtie2 -p 4 -x /home/wrtlbrnft/RNAseq/yeast_refSeq/idx/S288c -1 *_1.fq -2 *_2.fq --end-to-end -S align.sam
+bowtie2 -p $threads -x /home/wrtlbrnft/RNAseq/yeast_refSeq/idx/S288c --end-to-end -1 *_1.fq.gz -2 *_2.fq.gz > align.bam
+echo "Finished running bowtie2"
+
+# Get stats
+samtools flagstat align.bam > flagstat.txt
+echo "Finished getting flagstats"
 
 # Create corresponding .bam file and sort it
-samtools view -b align.sam > align.bam
+#samtools view -b align.sam > align.bam
 samtools sort -o align_sort.bam align.bam
+echo "Finished sorting align.bam"
 
 # run cufflinks and store results in cufflinks folder
 mkdir ./cufflinks
-cufflinks -g /home/wrtlbrnft/RNAseq/GCF_000146045.2_R64.ncbiRefSeq.gtf -o ./cufflinks -p 4 -L $letter align_sort.bam
+cufflinks -g /home/wrtlbrnft/RNAseq/GCF_000146045.2_R64.ncbiRefSeq.gtf -o ./cufflinks -p $threads -L $letter align_sort.bam
+echo "Finished transcript assembly (Cufflinks)"
 
 # compare to reference genome and move results to cmp subfolder
 cuffcompare -r /home/wrtlbrnft/RNAseq/GCF_000146045.2_R64.ncbiRefSeq.gtf -R -o cmp cufflinks/transcripts.gtf
+echo "Finished comparison to reference genome (Cuffcompare)"
+echo "All done!"
--- get_stats1.sh
+++ get_stats1.sh
@@ -6,15 +6,15 @@ do
 	echo "\n"$file >> stats.txt
 	echo "QC-passed reads:" >> stats.txt
 	# samtools flagstat has been run and output saved in flagstat
-	head -n 1 A/flagstat.txt | cut -d " " -f1 >> stats.txt
+	head -n 1 $file/flagstat.txt | cut -d " " -f1 >> stats.txt
 
 	# line 7 contains #alignments
 	echo "Alignments:" >> stats.txt
-	head -n 7 A/flagstat.txt | tail -n 1 | cut -d " " -f1 >> stats.txt
+	head -n 7 $file/flagstat.txt | tail -n 1 | cut -d " " -f1 >> stats.txt
 
 	# line 12 contains #properly paired alignments
 	echo "Properly paired:" >> stats.txt
-	head -n 12 A/flagstat.txt | tail -n 1 | cut -d " " -f1 >> stats.txt
+	head -n 12 $file/flagstat.txt | tail -n 1 | cut -d " " -f1 >> stats.txt
 
 	# Cufflinks output
 	echo "\nTranscript assembly statistics" >> stats.txt
